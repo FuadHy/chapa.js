d065c26c7aba682a643465f75f4023f4ec864dd6
nabroleonx <nabroleonx@gmail.com>
2022-08-26 17:31:17 +0300

    Migrate from prototypes to ES6 classes

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
-const Chapa = require('./index')
+import { Chapa } from "./index.js";
 
-let myChapa = new Chapa('12345')
-myChapa.initialize({
-    'amount': '100',
-    'currency': 'ETB',
-    'email': 'abebe@bikila.com',
-    'first_name': 'Abebe',
-    'last_name': 'Bikila',
-    'tx_ref': 'tx-myecommerce12345',
-    'callback_url': 'https://chapa.co',
-    'customization[title]': 'I love e-commerce',
-    'customization[description]': 'It is time to pay'
-}).then(d => console.log('ddd', d))
-.catch(e => console.log(e))
+let myChapa = new Chapa("12345");
+
+myChapa
+  .initialize({
+    amount: "100",
+    currency: "ETB",
+    email: "abebe@bikila.com",
+    first_name: "Abebe",
+    last_name: "Bikila",
+    tx_ref: "tx-myecommerce12345",
+    "customization[title]": "I love e-commerce",
+    "customization[description]": "It is time to pay",
+  })
+  .then((res) => console.log(res))
+  .catch((error) => console.log(error));
 
 // myChapa.verify('e3e3e3e3e').then(d => console.log(d)).catch(e => console.log(e))
